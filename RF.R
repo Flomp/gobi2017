ddcd767bf0546f842b5be346b89485a99ffb3803
Lu96 <luise.schuller@tum.de>
2017-02-24 17:50:53 +0100

    Add files via upload

--- RF.R
+++ RF.R
@@ -6,8 +6,9 @@ library(randomForest)
 # am besten RF mehrfach ausfuehren
 
 gene_count_rf <- data.frame(outcome = as.character(phenotype_labels[,1]), data.matrix(gene_counts_random))
+junction_count_rf <- data.frame(outcome2 = as.character(phenotype_labels[,1]), t(data.matrix(junction_counts_random)))
 
-junction_count_rf <- data.frame(outcome = as.character(phenotype_labels[,1]), t(data.matrix(junction_counts_random)))
+####### RF fuer Genes
 outcome <- gene_count_rf$outcome
 geneModel <- randomForest(outcome~.,data=gene_count_rf, importance = TRUE, ntree=1000, replace = TRUE, do.trace = TRUE, keep.forest=TRUE)
 
@@ -21,6 +22,7 @@ mean(geneModel$oob.times)
 table(gene_count_rf$outcome, geneModel$predicted)
 
 # OOB-Fehlerschaetzung statt Kreuzvalidierung
+
 geneModel$err.rate
 
 #geneModel$err.rate[100,1]
@@ -36,9 +38,9 @@ vorhersage_geneModel<- table(gene_count_rf$outcome,vorhersage)
 #Variablenwichtigkeit
 imp <- varImpPlot(geneModel)
 
-
+######### Junction RF
 # Wald Junctions generieren
-outcome2 <- junction_count_rf$outcome
+outcome2 <- junction_count_rf$outcome2
 junctionModel <- randomForest(outcome2~.,data=junction_count_rf, importance = TRUE, ntree=1000, replace = TRUE, do.trace = TRUE, keep.forest=TRUE)
 
 junctionModel
@@ -49,20 +51,20 @@ summary(junctionModel)
 junctionModel$obb.times
 mean(junctionModel$oob.times)
 
-table(junction_count_rf$outcome, junctionModel$predicted)
+table(junction_count_rf$outcome2, junctionModel$predicted)
 
 # OOB-Fehlerschaetzung statt Kreuzvalidierung
-junction_count_rf$err.rate
+junctionModel$err.rate
 
 #geneModel$err.rate[100,1]
-plot(junction_count_rf$err.rate[,1], type="l")
+plot(junctionModel$err.rate[,1], type="l")
 
 
 #Vorhersage Datensatz
 vorhersage_junction <- predict(junctionModel, newdata=junction_count_rf)
-vorhersage_junctionModel<- table(junction_count_rf$outcome,vorhersage_junction)
+vorhersage_junctionModel<- table(junction_count_rf$outcome2,vorhersage_junction)
 
-(vorhersage_junctionModel[1,1]+vorhersage_junctionModel[2,2])/length(junction_count_rf$outcome) # Accuracy
+(vorhersage_junctionModel[1,1]+vorhersage_junctionModel[2,2])/length(junction_count_rf$outcome2) # Accuracy
 
 #Variablenwichtigkeit
 imp <- varImpPlot(junctionModel)
